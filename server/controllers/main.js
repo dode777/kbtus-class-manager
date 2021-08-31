529274411bb8b4ffb45f4ad156450e88dfff3cf2
Dohae <dode777.dev@gmail.com>
2021-08-31 18:22:06 +0900

    feat: 학번, 이름, 수강정보 내용 출력 구현
    browser.close()로 브라우저 닫아줘야 send of null 오류가 발생하지 않을듯 함

--- server/controllers/main.js
+++ server/controllers/main.js
@@ -8,8 +8,12 @@ const getMainData = async (req, res) => {
       headless: false,
     });
 
-    const id = process.env.id;
-    const pw = process.env.pw;
+    const id = process.env.KBTUS_ID;
+    const pw = process.env.KBTUS_PW;
+    const url = 'https://truth.kbtus.ac.kr:8443/KBTUS_HAKSA/index.jsp';
+    let img = 'captchaImg.png';
+
+    let isLoggedIn = false;
 
     const page = (await browser.pages())[0];
 
@@ -18,72 +22,102 @@ const getMainData = async (req, res) => {
       height: 500,
     });
 
-    const url = 'https://truth.kbtus.ac.kr:8443/KBTUS_HAKSA/index.jsp';
     await page.goto(url);
-
-    // await page.type(String.fromCharCode(13));
-
-    // 보안문자 캡쳐
-    await page.screenshot({
-      path: `public/captchaImg.png`,
-      //   fullPage: true,
-      clip: { x: 54, y: 340, width: 81, height: 48 },
-    });
-
-    // 캡쳐한 보안문자 인식
-    let img = 'captchaImg.png';
-
     await worker.load();
-    await worker.loadLanguage('eng');
-    await worker.initialize('eng');
-    await worker.setParameters({
-      tessedit_char_whitelist: '0123456789',
-    });
-    const {
-      data: { text },
-    } = await worker.recognize(img);
-    await worker.terminate();
 
-    // 결과값에서 숫자만 추출
-    const regex = /[^0-9]/g;
-    let result = text.replace(regex, '');
-
-    // 웹에 데이터 입력
-    await page.evaluate(
-      (id, pw, result) => {
-        document.querySelector('input[name="V_USER_ID"]').value = id;
-        document.querySelector('input[name="V_USER_PWD"]').value = pw;
-        document.querySelector('input[name="txtCaptcha"]').value = result;
-      },
-      id,
-      pw,
-      result
-    );
-
-    // 로그인 버튼 클릭
-    await page.click('a[class="btn_login"]');
-
-    await page.waitForTimeout(3000);
-
-    if (page.url() == 'https://truth.kbtus.ac.kr:8443/KBTUS_HAKSA/mainPage.do') {
-      await page.click('a.btn_menu_open');
-      //   await page.waitForTimeout(500);
-      await page.click('li.mn_LESN > a.bgbt');
-      //   await page.waitForTimeout(500);
-      await page.click('li.mn_LESN > ul > li.fd:nth-child(1)');
-
-      await page.waitForSelector('.content .box_search p input#S_HAKBUN');
-      let data = await page.$eval('.content .box_search p input#S_HAKBUN', (element) => {
-        return element;
+    // 로그인 실패 시 While문으로 반복
+    while (!isLoggedIn) {
+      await page.waitForTimeout(2000);
+      // 보안문자 캡쳐
+      await page.screenshot({
+        path: `./captchaImg.png`,
+        clip: { x: 54, y: 340, width: 81, height: 48 },
       });
 
-      // 결과값 전송
-      res.send(data);
-    } else {
-      res.status(401).send({ message: 'Login Fail' });
+      // 캡쳐한 보안문자 인식
+      await worker.loadLanguage('eng');
+      await worker.initialize('eng');
+      await worker.setParameters({
+        tessedit_char_whitelist: '0123456789',
+      });
+      let {
+        data: { text },
+      } = await worker.recognize(img);
 
-      // 페이지 닫기
-      await page.close();
+      // 결과값에서 숫자만 추출
+      const regex = /[^0-9]/g;
+      let result = text.replace(regex, '');
+
+      if (result.length !== 4) {
+        console.log('보안문자 4자리 이하');
+        await page.reload(url);
+        await page.waitForSelector('a[class="btn_login"]');
+        continue;
+      }
+
+      // 웹에 데이터 입력
+      await page.waitForSelector('input[name="V_USER_ID"]', 'input[name="V_USER_PWD"]', 'input[name="txtCaptcha"]');
+      // await page.waitForSelector('input[name="V_USER_PWD"]');
+      // await page.waitForSelector('input[name="txtCaptcha"]');
+      await page.evaluate(
+        (id, pw, result) => {
+          document.querySelector('input[name="V_USER_ID"]').value = id;
+          document.querySelector('input[name="V_USER_PWD"]').value = pw;
+          document.querySelector('input[name="txtCaptcha"]').value = result;
+        },
+        id,
+        pw,
+        result
+      );
+
+      // 로그인 버튼 클릭
+      await page.waitForSelector('a[class="btn_login"]');
+      await page.click('a[class="btn_login"]');
+
+      await page.waitForTimeout(2000);
+
+      // 로그인 성공 시 데이터 크롤링 작업
+      if (page.url() == 'https://truth.kbtus.ac.kr:8443/KBTUS_HAKSA/mainPage.do') {
+        isLoggedIn = true;
+
+        await worker.terminate();
+
+        await page.waitForSelector('a.btn_menu_open');
+        await page.click('a.btn_menu_open');
+        await page.waitForSelector('li.mn_LESN > a.bgbt');
+        await page.click('li.mn_LESN > a.bgbt');
+        await page.waitForSelector('li.mn_LESN > ul > li.fd:nth-child(1)');
+        await page.click('li.mn_LESN > ul > li.fd:nth-child(1)');
+
+        await page.waitForTimeout(2000);
+
+        const frame = page.frames().find((f) => f.name() === 'frame300017');
+
+        // 학번, 이름
+        const getHakbun = await frame.$('#S_HAKBUN');
+        const getName = await frame.$('#S_NAME');
+
+        // 이번 학기 수업 수강 정보
+        let lectures = [];
+
+        await frame.waitForTimeout(3000);
+        const getLectures = await frame.$$eval('#dataGrid > tbody > tr > td', (data) => {
+          return data.map((data) => data.textContent);
+        });
+
+        for (i = 0; getLectures.length > i; i += 9) {
+          lectures.push([getLectures[i], getLectures[i + 2], getLectures[i + 3], getLectures[i + 4]]);
+        }
+
+        const user_hakbun = await frame.evaluate((getHakbun) => getHakbun.value, getHakbun);
+        const user_name = await frame.evaluate((getName) => getName.value, getName);
+
+        // 결과값 전송
+        res.send({ 이름: user_name, 학번: user_hakbun, 강의정보: lectures });
+        break;
+      }
+      await page.reload(url);
+      continue;
     }
   } catch (error) {
     console.log(error);
